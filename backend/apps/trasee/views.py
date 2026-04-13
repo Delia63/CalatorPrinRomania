49b2b71ed97397a727b409f99600ba7c8939964f
Delia63 <deliastoica03@yahoo.com>
2026-04-13 17:27:18 +0300

    back preparate + festivaluri afisare

--- backend/apps/trasee/views.py
+++ backend/apps/trasee/views.py
@@ -6,6 +6,8 @@ from rest_framework.response import Response
 from .models import Traseu
 from .serializers import TraseuSerializer
 from apps.atractii.models import AtractieTuristica
+from apps.regiuni.models import Festival, PreparatLocal
+from datetime import datetime
 import math
 
 def distanta_punct_la_linie(punct, linie):
@@ -108,12 +110,66 @@ class TraseuViewSet(viewsets.ModelViewSet):
                     'ratingMediu': float(atractie.ratingMediu),
                     'imagineCopertaUrl': atractie.imagineCopertaUrl,
                 })
+        
+        festivaluri_pe_traseu = []
+        data_start = request.data.get('dataStart')
+        data_end = request.data.get('dataEnd')
+        if data_start and data_end:
+            data_start = datetime.strptime(data_start, '%Y-%m-%d').date()
+            data_end = datetime.strptime(data_end, '%Y-%m-%d').date()
+            festivaluri = Festival.objects.filter(
+                dataStart__lte=data_end,
+                dataEnd__gte=data_start
+            )
+            for f in festivaluri:
+                festivaluri_pe_traseu.append({
+                    'id': f.id,
+                    'nume': f.nume,
+                    'dataStart': str(f.dataStart),
+                    'dataEnd': str(f.dataEnd),
+                })
+
+        regiuni_traseu = set()
+        route_coords = feature['geometry']['coordinates']
+        for coord in route_coords[::50]:
+            lon, lat = coord
+            if lat > 47.0:
+                regiuni_traseu.add('Maramureș')
+                regiuni_traseu.add('Moldova')
+            elif lat > 46.0 and lon < 24.0:
+                regiuni_traseu.add('Transilvania')
+            elif lat > 46.0 and lon > 26.0:
+                regiuni_traseu.add('Moldova')
+            elif lat > 45.5:
+                regiuni_traseu.add('Transilvania')
+            elif lat < 44.5 and lon > 25.0:
+                regiuni_traseu.add('Muntenia')
+            elif lat < 45.5 and lon < 24.0:
+                regiuni_traseu.add('Oltenia')
+            elif lon > 28.0:
+                regiuni_traseu.add('Dobrogea')
+            else:
+                regiuni_traseu.add('Muntenia')
+                regiuni_traseu.add('Transilvania')
+
+        preparate_pe_traseu = []
+        if regiuni_traseu:
+                preparate = PreparatLocal.objects.filter(regiune__in = regiuni_traseu)
+
+                for p in preparate:
+                    preparate_pe_traseu.append({
+                        'id': p.id,
+                        'nume': p.nume,
+                        'regiune': p.regiune,
+                    })
 
         res_data = {
             'geojson': ruta,
             'distantaKm': round(summary['distance'] / 1000, 2),
             'durataMin': round(summary['duration'] / 60),
             'atractii': atractii_pe_traseu,
+            'festivaluri': festivaluri_pe_traseu,
+            'preparate': preparate_pe_traseu,
         }
 
         if request.user.is_authenticated:
